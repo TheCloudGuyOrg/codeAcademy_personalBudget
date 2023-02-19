f732100f506bb9604fe51e4286f88211d6030313
thecloudguyllc <derek.ryan.cox@thecloudguy.org>
2023-02-19 15:23:46 -0700

    Removing unneeded modules from dbqueries

--- database/dbqueries.js
+++ database/dbqueries.js
@@ -1,8 +1,6 @@
-const express = require('express')
-const app = express()
 const db = require('./dbconnection.js')
 
-
+//Queries
 const getEnvelopes = (request, response) => {
     db.query('SELECT * FROM envelopes ORDER BY id ASC', (error, results) => {
       if (error) {
@@ -14,7 +12,6 @@ const getEnvelopes = (request, response) => {
 
 const getEnvelopesById = (request, response) => {
     const id = parseInt(request.params.id)
-  
     db.query('SELECT * FROM envelopes WHERE id = $1', [id], (error, results) => {
       if (error) {
         throw error
