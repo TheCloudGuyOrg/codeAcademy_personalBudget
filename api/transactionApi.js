51d975cf92d748cc8a4822c36beec5f904056db4
thecloudguyllc <derek.ryan.cox@thecloudguy.org>
2023-02-20 20:20:47 -0700

    Adding Transaction Skeleton

---
+++ api/transactionApi.js
@@ -0,0 +1,3 @@
+//Update your database to add a transactions feature to your project.
+
+//Transactions should include a date, payment amount, payment recipient, and designated envelope from which to remove funds
