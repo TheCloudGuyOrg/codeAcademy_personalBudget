695edbf07231123bd81d22bd84546dfa36cbbf78
thecloudguyllc <derek.ryan.cox@thecloudguy.org>
2023-03-06 14:47:04 -0700

    Adding notes for tests to be completed

--- test/index_test.js
+++ test/index_test.js
@@ -9,3 +9,15 @@ describe('description', () => {
         // Teardown
     });
   });
+
+  //Test getEnvelopes
+  //Test getEnvelopes by ID
+  //Test addEnvelopes
+  //Test updateEnvelopes
+  //Test deleteEnvelopes
+  //Test addEnvelopeTransaction
+  //Test getEnvelopeTransaction
+  //Test getTransactions
+  //Test getTransactions by ID
+  //Test updateTransactions
+  //Test deleteTransactions
